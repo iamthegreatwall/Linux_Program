b3e4f0e5fab344ef92d1e06c354447f4048239cb
iamthegreatwall <1747634351@qq.com>
2021-10-03 14:36:45 +0800

    多次创建进程

--- MultiProcess/ForkTwoTimes.c
+++ MultiProcess/ForkTwoTimes.c
@@ -5,7 +5,7 @@
 int main(void)
 {
     /*
-    一个进程fork衣蛾子进程，但不要他等待子进程种植，也不希望子进程处于
+    一个进程fork一个子进程，但不要他等待子进程终止，也不希望子进程处于
     僵死状态直到父进程终止，可以通过调用fork两次来实现
     */
     pid_t pid;
@@ -28,11 +28,12 @@ int main(void)
             printf("第一代子进程：pid=%ld\n",(long)getpid());
             exit(EXIT_SUCCESS);/*first child*/
         }
-        printf("第一代子进程：pid=%ld\n",(long)getpid());
+        
         printf("第二代子进程：pid=%ld\n",(long)getpid());
         printf("第二代子进程（未睡眠前）：ppid=%ld\n",(long)getppid());
         sleep(2);
         printf("second child,parent pid=%ld\n",(long)getppid());
+        exit(EXIT_SUCCESS);
     }
     if(waitpid(pid,NULL,0)!=pid)
     {
